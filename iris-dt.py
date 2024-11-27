464f7c8af676ca86d7762989728847dd56d38ee3
Anshul patle <patleanshul920@gmail.com>
2024-11-28 02:06:45 +0530

    commit dagsflow config

--- iris-dt.py
+++ iris-dt.py
@@ -16,11 +16,6 @@ dagshub.init(repo_owner='Anshul-0807', repo_name='Mlflow-dagshub-demo', mlflow=T
 
 mlflow.set_tracking_uri('https://dagshub.com/Anshul-0807/Mlflow-dagshub-demo.mlflow')
 
-
-# code log
-
-mlflow.log_artifact(__file__)
-
 # Load the iris dataset
 iris = load_iris()
 X = iris.data
@@ -30,47 +25,45 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define the parameters for the Random Forest model
-max_depth = 15
-
-# set experiment
-
-mlflow.set_experiment('iris-dt')
+max_depth = 1
 
 # apply mlflow
+
+mlflow.set_experiment('iris-dt')
 
 with mlflow.start_run():
 
     dt = DecisionTreeClassifier(max_depth=max_depth)
 
-    dt.fit(X_train,y_train)
+    dt.fit(X_train, y_train)
 
     y_pred = dt.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
 
-    # mlflow logging 
-
     mlflow.log_metric('accuracy', accuracy)
+
     mlflow.log_param('max_depth', max_depth)
 
-     # Create a confusion matrix plot
+    # Create a confusion matrix plot
     cm = confusion_matrix(y_test, y_pred)
     plt.figure(figsize=(6,6))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
     plt.ylabel('Actual')
     plt.xlabel('Predicted')
     plt.title('Confusion Matrix')
-
-
+    
     # Save the plot as an artifact
     plt.savefig("confusion_matrix.png")
 
     # mlflow code
     mlflow.log_artifact("confusion_matrix.png")
 
-    # -------
+    mlflow.log_artifact(__file__)
 
-    print("accuracy :" ,accuracy)
-    
+    mlflow.sklearn.log_model(dt, "decision tree")
 
+    mlflow.set_tag('author','nitish')
+    mlflow.set_tag('model','decision tree')
 
+    print('accuracy', accuracy)
